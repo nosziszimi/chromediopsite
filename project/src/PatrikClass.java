ddf0de0e4e22e41ea4daa61b91c596061099c00e
Patrik <horvath.patrik001@gmail.com>
2017-06-07 14:05:10 +0200

    b5

--- project/src/PatrikClass.java
+++ project/src/PatrikClass.java
@@ -2,11 +2,11 @@
 public class PatrikClass {
 
   public static void main(String[] args) {
-    count("május", 2);
+    count("május ", 2);
   }
 
   private static void count(String givenParameter, int givennumber) {
-    System.out.println(givenParameter);
+    System.out.println(givenParameter + givennumber);
   }
 
 }
