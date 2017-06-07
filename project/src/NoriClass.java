f612dc093677e0e1dc8151036a1210b4cf798969
Daniel Szalay <sza.dan.uj@gmail.com>
2017-06-07 14:20:35 +0200

    d6

--- project/src/NoriClass.java
+++ project/src/NoriClass.java
@@ -1,6 +1,3 @@
-/**
- * Created by Nóra on 2017. 06. 07..
- */
 public class NoriClass {
 
   public static void main(String[] args) {
--- project/src/PetiClass.java
+++ project/src/PetiClass.java
@@ -1,13 +1,10 @@
-/**
- * Created by peter on 2017.06.07..
- */
 public class PetiClass {
 
   public static void main(String[] args) {
     cheer("Java");
   }
 
-  private static void cheer(String toSystemOut) {
-    System.out.println(toSystemOut);
+  private static void cheer(String parameter) {
+    System.out.println("Cheer to " + parameter);
   }
 }
