7650a8ec8a21ed2f155c85046476a7c426936b16
Patrik <horvath.patrik001@gmail.com>
2017-06-07 11:44:26 +0200

    b1

---
+++ src/PatrikClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Lenovo on 2017.06.07..
+ */
+public class PatrikClass {
+
+  public static void main(String[] args) {
+  count();
+  }
+
+  public static void count() {
+    System.out.println("24");
+  }
+}
